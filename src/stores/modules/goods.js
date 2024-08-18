009bde98afce602896f463ecd3a01ca6cea5a7b1
crimsonvan <2329016850@qq.com>
2024-08-18 15:35:05 +0800

    goodjs的数字完成

--- src/stores/modules/goods.js
+++ src/stores/modules/goods.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useNumStore = defineStore(
   'num',
   () => {
-    const count = ref(9900)
+    const count = ref(0)
     const avatar = ref(
       'https://mp-e0d15f0f-d6bf-4f95-b183-b82aede04535.cdn.bspapp.com/可爱蜘蛛侠.png'
     )
