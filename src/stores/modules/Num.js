7742456d13659dd7274dace67917040fa7276f34
crimsonvan <2329016850@qq.com>
2024-08-18 14:41:39 +0800

    完成了第三次提交vue

--- src/stores/modules/Num.js
+++ src/stores/modules/Num.js
@@ -8,6 +8,7 @@ export const useNumStore = defineStore(
     const avatar = ref(
       'https://mp-e0d15f0f-d6bf-4f95-b183-b82aede04535.cdn.bspapp.com/可爱蜘蛛侠.png'
     )
+
     const doubleCount = computed(() => count.value * 2)
     function increment() {
       count.value++
