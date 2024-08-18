ba42ac83c9a641a71836a1940652099a2d0774dd
crimsonvan <2329016850@qq.com>
2024-08-18 15:30:27 +0800

    完成了第七次提交vue

--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore(
   'num',
   () => {
-    const count = ref(0)
+    const count = ref(12123)
     const avatar = ref(
       'https://mp-e0d15f0f-d6bf-4f95-b183-b82aede04535.cdn.bspapp.com/可爱蜘蛛侠.png'
     )
